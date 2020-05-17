63dc6b4df680f66f29484972a1b9aa94c2756b3a
Jakub Dąbek <jakub.dabek@gmail.com>
2020-05-17 19:40:06 +0200

    Add basic geometry classes

---
+++ src/model/geom/track.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from typing import List, Tuple, Iterable
+
+from planar import Vec2
+from planar.line import LineSegment
+
+from model.geom.segment import Segment
+from model.geom.wall import Wall
+from utils import pairwise
+
+
+@dataclass
+class Track:
+    segments: List[Segment]
+
+    @classmethod
+    def from_points(cls, points: Iterable[Tuple[Vec2, Vec2]]) -> "Track":
+        segments: List[Segment] = []
+        for (s1, s2), (e1, e2) in pairwise(points):
+            segments.append(
+                Segment(
+                    Wall(LineSegment.from_points((s1, e1))),
+                    Wall(LineSegment.from_points((s2, e2))),
+                )
+            )
+
+        return cls(segments)
---
+++ src/utils.py
@@ -0,0 +1,12 @@
+from typing import Iterable, Tuple, TypeVar
+import itertools
+
+
+T = TypeVar("T")
+
+
+def pairwise(iterable: Iterable[T]) -> Iterable[Tuple[T, T]]:
+    """s -> (s0,s1), (s1,s2), (s2, s3), ..."""
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
