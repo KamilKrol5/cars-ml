8f4f33cfe28ac9cae56320b48b6354785c74e5c5
Kamil <kamil55525@gmail.com>
2020-05-17 12:53:04 +0200

    Add basic activation functions

---
+++ src/model/neural_network/activation_functions.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+tanh = np.tanh
+
+
+def tanh_derivative(x: np.ndarray) -> np.ndarray:
+    return 1.0 - np.tanh(x) ** 2
+
+
+def sigmoid(x: np.ndarray) -> np.ndarray:
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_star(x: np.ndarray) -> np.ndarray:
+    return x * (1.0 - x)
+
+
+def relu(x: np.ndarray) -> np.ndarray:
+    return np.maximum(0, x)
+
+
+def relu_derivative(x: np.ndarray) -> np.ndarray:
+    return np.where(x > 0, 1, 0)
