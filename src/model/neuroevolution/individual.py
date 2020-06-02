71ddc2ead50caefcfa28e38cf6d7595f4e9ca737
Kamil <kamil55525@gmail.com>
2020-06-02 21:41:31 +0200

    Genetic algorithm code refactor
    Moved mutation and reproduction related things to corresponding Individual classes.
    Now individuals are split into two types: child and normal (adult);
    It prevents from accessing adaptation field when it is undefined or irrelevant.
    Added use of Environment class. Useless Neuroevolution init arguments were removed.

--- src/model/neuroevolution/individual.py
+++ src/model/neuroevolution/individual.py
@@ -1,30 +1,220 @@
-from typing import Optional, Tuple
+from copy import deepcopy
+from dataclasses import dataclass
+from typing import Tuple, ClassVar, List, Callable, cast
 
 import numpy as np
 
 from model.neural_network.neural_network import NeuralNetwork, Layer
 
 
-class Individual:
-    def __init__(self, neural_network: NeuralNetwork):
-        self._neural_network: NeuralNetwork = neural_network
-        self.adaptation: Optional[int] = None
+@dataclass
+class ChildIndividual:
+    neural_network: NeuralNetwork
 
-    @property
-    def neural_network(self) -> NeuralNetwork:
-        return self._neural_network
+    available_mutations: ClassVar[List[Callable[["ChildIndividual"], None]]]
 
     def get_random_layer(self) -> Layer:
         random_layer: Layer = np.random.choice(self.neural_network.hidden_layers)
         return random_layer
 
     def get_random_layer_index(self) -> int:
-        return np.random.randint(low=0, high=len(self.neural_network.hidden_layers))
+        return cast(
+            int, np.random.randint(low=0, high=len(self.neural_network.hidden_layers))
+        )
 
     @staticmethod
-    def get_random_weight_index(neural_network_layer: Layer) -> Tuple:
+    def get_random_weight_index(neural_network_layer: Layer) -> Tuple[int, ...]:
         return tuple(np.random.randint(neural_network_layer.weights.shape))
 
     @staticmethod
     def get_random_bias_index(neural_network_layer: Layer) -> int:
-        return np.random.randint(neural_network_layer.biases.shape[0])
+        return cast(int, np.random.randint(neural_network_layer.biases.shape[0]))
+
+    @staticmethod
+    def random_weight_mutation(individual: "ChildIndividual") -> None:
+        """
+        Changes a randomly chosen weight in the neural network to random value from range [-1, 1)
+
+        :param individual: Individual (neural network) to be modified.
+        :return: None
+        """
+        random_layer = individual.get_random_layer()
+        random_index = individual.get_random_weight_index(random_layer)
+        # TODO discuss new weight value's range, update method documentation
+        random_layer.weights[random_index] = np.random.uniform(-1, 1)
+
+    @staticmethod
+    def random_bias_mutation(individual: "ChildIndividual") -> None:
+        """
+        Changes a randomly chosen bias in the neural network to random value from range [-1, 1)
+
+        :param individual: Individual (neural network) to be modified.
+        :return: None
+        """
+        random_layer = individual.get_random_layer()
+        random_index = individual.get_random_bias_index(random_layer)
+        # TODO discuss new bias value's range, update method documentation
+        random_layer.biases[random_index] = np.random.uniform(-1, 1)
+        pass
+
+    @staticmethod
+    def change_weight_sign_mutation(individual: "ChildIndividual") -> None:
+        """
+        Changes a sign of randomly chosen weight in the neural network.
+
+        :param individual: Individual (neural network) to be modified.
+        :return: None
+        """
+        random_layer = individual.get_random_layer()
+        random_index = individual.get_random_weight_index(random_layer)
+        random_layer.weights[random_index] = -random_layer.weights[random_index]
+
+    @staticmethod
+    def multiply_neuron_weights_mutation(individual: "ChildIndividual") -> None:
+        """
+        Multiplies all weights for single randomly chosen neuron,
+        from randomly chosen layer, by random numbers from the range [0.5, 1.5].
+        There is a random multiplier chosen for every single weight.
+        :param individual: Individual to be changed.
+        :return: None
+        """
+        random_layer = individual.get_random_layer()
+        random_neuron_index = np.random.randint(0, random_layer.weights.shape[0])
+        multipliers = np.random.uniform(
+            low=0.5, high=1.5, size=(random_layer.weights.shape[1])
+        )
+        random_layer.weights[random_neuron_index] *= multipliers
+
+    @staticmethod
+    def random_neuron_weights_mutation(individual: "ChildIndividual") -> None:
+        """
+        Changes all weights for single randomly chosen neuron from randomly chosen layer.
+        :param individual: Individual to be changed.
+        :return: None
+        """
+        random_layer = individual.get_random_layer()
+        random_neuron_index = np.random.randint(0, random_layer.weights.shape[0])
+        # TODO decide the range of new random weights
+        random_layer.weights[random_neuron_index] = np.random.random(
+            (random_layer.weights.shape[1])
+        )
+
+
+ChildIndividual.available_mutations = [
+    ChildIndividual.random_weight_mutation,
+    ChildIndividual.random_bias_mutation,
+    ChildIndividual.change_weight_sign_mutation,
+    ChildIndividual.multiply_neuron_weights_mutation,
+    ChildIndividual.random_neuron_weights_mutation,
+]
+
+
+@dataclass
+class Individual:
+    neural_network: NeuralNetwork
+    adaptation: float
+
+    available_reproductions: ClassVar[
+        List[
+            Callable[
+                ["Individual", "Individual"], Tuple[ChildIndividual, ChildIndividual]
+            ]
+        ]
+    ]
+
+    @staticmethod
+    def weight_swap_reproduction(
+        father1: "Individual", father2: "Individual", weights_to_be_swapped: int = 1
+    ) -> Tuple[ChildIndividual, ChildIndividual]:
+        """
+        Return two new individuals (children). They are created by
+        swapping one or more parents's weights.
+
+        :param father1: First parent
+        :param father2: Second parent
+        :param weights_to_be_swapped: number of weights to be swapped
+        :return: Tuple consisting two new individuals.
+        """
+        child_1 = ChildIndividual(deepcopy(father1.neural_network))
+        child_2 = ChildIndividual(deepcopy(father2.neural_network))
+        for _ in range(weights_to_be_swapped):
+            random_layer_index = child_1.get_random_layer_index()
+            child_1_rand_layer = child_1.neural_network.hidden_layers[
+                random_layer_index
+            ]
+            child_2_rand_layer = child_2.neural_network.hidden_layers[
+                random_layer_index
+            ]
+            weight_index = ChildIndividual.get_random_weight_index(child_1_rand_layer)
+            (
+                child_1_rand_layer.weights[weight_index],
+                child_2_rand_layer.weights[weight_index],
+            ) = (
+                child_2_rand_layer.weights[weight_index],
+                child_1_rand_layer.weights[weight_index],
+            )
+        return child_1, child_2
+
+    @staticmethod
+    def neuron_swap_reproduction(
+        mother1: "Individual", mother2: "Individual", neurons_to_swap: int = 1
+    ) -> Tuple[ChildIndividual, ChildIndividual]:
+        """
+        Return two new individuals (children). They are created by
+        swapping one or more randomly chosen neurons.
+
+        :param mother1: First parent
+        :param mother2: Second parent
+        :param neurons_to_swap: Number of neurons to be swapped
+        :return: Tuple consisting two new individuals.
+        """
+        child_1 = ChildIndividual(deepcopy(mother1.neural_network))
+        child_2 = ChildIndividual(deepcopy(mother2.neural_network))
+        for _ in range(neurons_to_swap):
+            random_layer_index = child_1.get_random_layer_index()
+            child_1_rand_layer_w = child_1.neural_network.hidden_layers[
+                random_layer_index
+            ].weights
+            child_2_rand_layer_w = child_2.neural_network.hidden_layers[
+                random_layer_index
+            ].weights
+            neuron_index = np.random.randint(0, child_2_rand_layer_w.shape[0])
+            child_1_rand_layer_w[neuron_index], child_2_rand_layer_w[neuron_index] = (
+                child_2_rand_layer_w[neuron_index],
+                child_1_rand_layer_w[neuron_index].copy(),
+            )
+        return child_1, child_2
+
+    @staticmethod
+    def layer_swap_reproduction(
+        mother: "Individual", father: "Individual", layers_to_swap: int = 1
+    ) -> Tuple[ChildIndividual, ChildIndividual]:
+        """
+        Return two new individuals (children). They are created by
+        swapping one or more randomly chosen layers.
+        All data from one layer (weights and biases) is swapped
+        with all data from another layer.
+
+        :param mother: First parent
+        :param father: Second parent
+        :param layers_to_swap: Number of layers to be swapped
+        :return: Tuple consisting two new individuals.
+        """
+        child_1 = ChildIndividual(deepcopy(mother.neural_network))
+        child_2 = ChildIndividual(deepcopy(father.neural_network))
+        for _ in range(layers_to_swap):
+            layer_index = child_1.get_random_layer_index()
+            child_1_rand_layers = child_1.neural_network.hidden_layers
+            child_2_rand_layers = child_2.neural_network.hidden_layers
+            child_2_rand_layers[layer_index], child_1_rand_layers[layer_index] = (
+                child_1_rand_layers[layer_index],
+                child_2_rand_layers[layer_index],
+            )
+        return child_1, child_2
+
+
+Individual.available_reproductions = [
+    Individual.weight_swap_reproduction,
+    Individual.neuron_swap_reproduction,
+    Individual.layer_swap_reproduction,
+]
--- src/model/neuroevolution/neuroevolution.py
+++ src/model/neuroevolution/neuroevolution.py
@@ -1,10 +1,10 @@
-from copy import deepcopy
-from typing import Tuple, Dict, Callable, List
+from typing import Tuple, List
 
 import numpy as np
 
+from model.environment.environment import Environment
 from model.neural_network.neural_network import NeuralNetwork, LayerInfo
-from model.neuroevolution.individual import Individual
+from model.neuroevolution.individual import Individual, ChildIndividual
 
 
 class Neuroevolution:
@@ -24,19 +24,12 @@ class Neuroevolution:
 
     @staticmethod
     def _reproduction_probability(
-        individual: Individual, bound_adaptation: int
+        individual: Individual, bound_adaptation: float
     ) -> float:
         """
         Probability of reproduction for every given individual
         without golden ticket according to bound adaptation.
         """
-        if individual.adaptation is None:
-            # this should never happen
-            raise Exception(
-                "Cannot compute probability of" "individual with undefined adaptation"
-            )
-        # thanks to mypy
-        assert isinstance(individual.adaptation, int)
         return (individual.adaptation / bound_adaptation) ** 3
 
     _MUTATION_CHANCE: float = 0.15
@@ -66,42 +59,30 @@ class Neuroevolution:
     _MUTATION_PROBABILITIES = np.array(_MUTATION_RATE) / sum(_MUTATION_RATE)
 
     def __init__(
-        self,
-        individuals: List[Individual],
-        layers_infos: List[LayerInfo],
-        output_neurons: int,
+        self, networks: List[ChildIndividual], environment: Environment,
     ) -> None:
-        self.individuals: List[Individual] = individuals
-        self._layers_infos = layers_infos
-        self._output_neurons = output_neurons
+        # TODO move environment somewhere else
+        self._environment: Environment = environment
+        self._new_generation: List[ChildIndividual] = networks
+        self.individuals: List[Individual] = []
         self._parents: List[Individual] = []
-        self._reproduce: Dict[int, Callable] = {
-            0: self._weight_swap_reproduction,
-            1: self._neuron_swap_reproduction,
-            2: self._layer_swap_reproduction,
-        }
-        self._mutate: Dict[int, Callable] = {
-            0: self._random_weight_mutation,
-            1: self._random_bias_mutation,
-            2: self._change_weight_sign_mutation,
-            3: self._multiply_neuron_weights_mutation,
-            4: self._random_neuron_weights_mutation,
-        }
 
     @classmethod
     def init_with_neural_network_info(
-        cls, layers_infos: List[LayerInfo], output_neurons: int
+        cls,
+        layers_infos: List[LayerInfo],
+        output_neurons: int,
+        environment: Environment,
     ) -> "Neuroevolution":
         return cls(
             [
-                Individual(NeuralNetwork(layers_infos, output_neurons))
+                ChildIndividual(NeuralNetwork(layers_infos, output_neurons))
                 for _ in range(cls._INDIVIDUALS)
             ],
-            layers_infos,
-            output_neurons,
+            environment,
         )
 
-    def _ranking(self) -> None:
+    def _sort_individuals(self) -> None:
         self.individuals.sort(
             key=lambda individual: individual.adaptation, reverse=True
         )
@@ -122,193 +103,42 @@ class Neuroevolution:
                 parents.append(self.individuals[i])
         return parents
 
-    @staticmethod
-    def _weight_swap_reproduction(
-        father1: Individual, father2: Individual, weights_to_be_swapped: int = 1
-    ) -> Tuple[Individual, Individual]:
-        """
-        Return two new individuals (children). They are created by
-        swapping one or more parents's weights.
-
-        :param father1: First parent
-        :param father2: Second parent
-        :return: Tuple consisting two new individuals.
-        """
-        child_1 = deepcopy(father1)
-        child_2 = deepcopy(father2)
-        for _ in range(weights_to_be_swapped):
-            random_layer_index = child_1.get_random_layer_index()
-            child_1_rand_layer = child_1.neural_network.hidden_layers[
-                random_layer_index
-            ]
-            child_2_rand_layer = child_2.neural_network.hidden_layers[
-                random_layer_index
-            ]
-            weight_index = Individual.get_random_weight_index(child_1_rand_layer)
-            (
-                child_1_rand_layer.weights[weight_index],
-                child_2_rand_layer.weights[weight_index],
-            ) = (
-                child_2_rand_layer.weights[weight_index],
-                child_1_rand_layer.weights[weight_index],
-            )
-        return child_1, child_2
-
-    @staticmethod
-    def _neuron_swap_reproduction(
-        mother1: Individual, mother2: Individual, neurons_to_swap: int = 1
-    ) -> Tuple[Individual, Individual]:
-        """
-        Return two new individuals (children). They are created by
-        swapping one or more randomly chosen neurons.
-
-        :param mother1: First parent
-        :param mother2: Second parent
-        :return: Tuple consisting two new individuals.
-        """
-        child_1 = deepcopy(mother1)
-        child_2 = deepcopy(mother2)
-        for _ in range(neurons_to_swap):
-            random_layer_index = child_1.get_random_layer_index()
-            child_1_rand_layer_w = child_1.neural_network.hidden_layers[
-                random_layer_index
-            ].weights
-            child_2_rand_layer_w = child_2.neural_network.hidden_layers[
-                random_layer_index
-            ].weights
-            neuron_index = np.random.randint(0, child_2_rand_layer_w.shape[0])
-            child_1_rand_layer_w[neuron_index], child_2_rand_layer_w[neuron_index] = (
-                child_2_rand_layer_w[neuron_index],
-                child_1_rand_layer_w[neuron_index].copy(),
-            )
-        return child_1, child_2
-
-    @staticmethod
-    def _layer_swap_reproduction(
-        mother: Individual, father: Individual, layers_to_swap: int = 1
-    ) -> Tuple[Individual, Individual]:
-        """
-        Return two new individuals (children). They are created by
-        swapping one or more randomly chosen layers.
-        All data from one layer (weights and biases) is swapped
-        with all data from another layer.
-
-        :param mother: First parent
-        :param father: Second parent
-        :return: Tuple consisting two new individuals.
-        """
-        child_1 = deepcopy(mother)
-        child_2 = deepcopy(father)
-        for _ in range(layers_to_swap):
-            layer_index = child_1.get_random_layer_index()
-            child_1_rand_layers = child_1.neural_network.hidden_layers
-            child_2_rand_layers = child_2.neural_network.hidden_layers
-            child_2_rand_layers[layer_index], child_1_rand_layers[layer_index] = (
-                child_1_rand_layers[layer_index],
-                child_2_rand_layers[layer_index],
-            )
-        return child_1, child_2
-
-    def _reproduction(self, parents: List[Individual]) -> List[Individual]:
+    def _reproduction(self, parents: List[Individual]) -> List[ChildIndividual]:
         children_remain = self._INDIVIDUALS - len(parents)
-        children = []
+        children: List[ChildIndividual] = []
         while children_remain > 0:
             mother, father = np.random.choice(parents, size=2)
             while mother == father:
                 father = np.random.choice(parents)
-            daughter, son = self._reproduce[
-                np.random.choice(
-                    list(self._reproduce.keys()), p=self._REPRODUCTION_PROBABILITIES
-                )
-            ](mother, father)
-            children.append(Individual(daughter))
+            daughter, son = np.random.choice(
+                Individual.available_reproductions, p=self._REPRODUCTION_PROBABILITIES
+            )(mother, father)
+            children.append(daughter)
             children_remain -= 1
             if children_remain > 0:
-                children.append(Individual(son))
+                children.append(son)
                 children_remain -= 1
         return children
 
-    @staticmethod
-    def _random_weight_mutation(individual: Individual) -> None:
-        """
-        Changes a randomly chosen weight in the neural network to random value from range [-1, 1)
-
-        :param individual: Individual (neural network) to be modified.
-        :return: None
-        """
-        random_layer = individual.get_random_layer()
-        random_index = individual.get_random_weight_index(random_layer)
-        # TODO discuss new weight value's range, update method documentation
-        random_layer.weights[random_index] = np.random.uniform(-1, 1)
-
-    @staticmethod
-    def _random_bias_mutation(individual: Individual) -> None:
-        """
-        Changes a randomly chosen bias in the neural network to random value from range [-1, 1)
-
-        :param individual: Individual (neural network) to be modified.
-        :return: None
-        """
-        random_layer = individual.get_random_layer()
-        random_index = individual.get_random_bias_index(random_layer)
-        # TODO discuss new bias value's range, update method documentation
-        random_layer.biases[random_index] = np.random.uniform(-1, 1)
-        pass
-
-    @staticmethod
-    def _change_weight_sign_mutation(individual: Individual) -> None:
-        """
-        Changes a sign of randomly chosen weight in the neural network.
-
-        :param individual: Individual (neural network) to be modified.
-        :return: None
-        """
-        random_layer = individual.get_random_layer()
-        random_index = individual.get_random_weight_index(random_layer)
-        random_layer.weights[random_index] = -random_layer.weights[random_index]
-
-    @staticmethod
-    def _multiply_neuron_weights_mutation(individual: Individual) -> None:
-        """
-        Multiplies all weights for single randomly chosen neuron,
-        from randomly chosen layer, by random numbers from the range [0.5, 1.5].
-        There is a random multiplier chosen for every single weight.
-        :param individual: Individual to be changed.
-        :return: None
-        """
-        random_layer = individual.get_random_layer()
-        random_neuron_index = np.random.randint(0, random_layer.weights.shape[0])
-        multipliers = np.random.uniform(
-            low=0.5, high=1.5, size=(random_layer.weights.shape[1])
-        )
-        random_layer.weights[random_neuron_index] *= multipliers
-
-    @staticmethod
-    def _random_neuron_weights_mutation(individual: Individual) -> None:
-        """
-        Changes all weights for single randomly chosen neuron from randomly chosen layer.
-        :param individual: Individual to be changed.
-        :return: None
-        """
-        random_layer = individual.get_random_layer()
-        random_neuron_index = np.random.randint(0, random_layer.weights.shape[0])
-        # TODO decide the range of new random weights
-        random_layer.weights[random_neuron_index] = np.random.random(
-            (random_layer.weights.shape[1])
-        )
-
-    def _mutation(self, individuals: List[Individual]) -> List[Individual]:
+    def _mutation(self, individuals: List[ChildIndividual]) -> List[ChildIndividual]:
         for individual in individuals:
             if np.random.rand() < self._MUTATION_CHANCE:
-                self._mutate[
-                    np.random.choice(
-                        list(self._mutate.keys()), p=self._MUTATION_PROBABILITIES
-                    )
-                ](individual)
+                np.random.choice(
+                    ChildIndividual.available_mutations, p=self._MUTATION_PROBABILITIES
+                )(individual)
         return individuals
 
     def evolve(self) -> None:
-        self._ranking()
-        parents = self._selection()
-        children = self._reproduction(parents)
-        self.individuals = parents + self._mutation(children)
+        adaptations = self._environment.compute_adaptations(
+            nn.neural_network for nn in self._new_generation
+        )
+        new_individuals = [
+            Individual(i.neural_network, adaptation)
+            for i, adaptation in zip(self._new_generation, adaptations)
+        ]
+        self.individuals.extend(new_individuals)
+        self._sort_individuals()
+        parents: List[Individual] = self._selection()
+        self.individuals = parents
+        children: List[ChildIndividual] = self._reproduction(parents)
+        self._new_generation = self._mutation(children)
