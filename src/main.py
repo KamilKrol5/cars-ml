8fbc9f2276bf6b4cfee99e0a751dc3eaa6b4f79d
Eryk <erykkrupa1@gmail.com>
2020-06-06 21:25:58 +0200

    Add storing and loading neural networks from file
    Removed old neural network class

--- src/main.py
+++ src/main.py
@@ -7,12 +7,13 @@ from planar.line import Ray
 from model.car import Car
 from model.geom.sensor import Sensor
 from model.geom.track import Track
-from model.neural_network_old import NeuralNetwork
 from model.simulation import Simulation
 from view.action import Action, ActionType
 from view.menu import Menu
 from view.track_view import TrackView
 from view.window import Window
+from model.neural_network.neural_network import NeuralNetwork, LayerInfo
+from model.neural_network.neural_network_store import NeuralNetworkStore
 
 WINDOW_NAME = "CarsML"
 WINDOW_SIZE = (1280, 800)
@@ -22,7 +23,20 @@ WINDOW_MIN_SIZE = (800, 600)
 def main_no_ui() -> None:
     from time import sleep
 
-    neural_network = NeuralNetwork()
+    neural_networks = []
+    for _ in range(100):
+        neural_networks.append(NeuralNetwork([
+            LayerInfo(1, "tanh"),
+            LayerInfo(15, "sigmoid"),
+            LayerInfo(10, "sigmoid")],
+            2))
+    NeuralNetworkStore.store(neural_networks, 'data')
+    neural_networks.clear()
+    neural_networks = NeuralNetworkStore.load('data')
+    print(len(neural_networks))
+
+    neural_network = neural_networks[0]
+
     sensor = Sensor(Ray((0, 0), (0, 1)))  # TODO: move construction elsewhere
 
     car = Car((1, 2), [sensor], neural_network)
@@ -38,7 +52,6 @@ def main_no_ui() -> None:
     while True:
         sleep(sleep_time)
         car.tick(track, sleep_time)
-        car.go_brrrr()
 
 
 def main() -> None:
@@ -75,4 +88,4 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    main_no_ui()
---
+++ src/model/neural_network/neural_network_store.py
@@ -0,0 +1,62 @@
+import pickle
+from os import makedirs
+from os.path import exists
+from typing import List
+
+from model.neural_network.neural_network import NeuralNetwork
+
+
+class NeuralNetworkStore:
+    """
+    Store and load neural networks from disc.
+    """
+    _DEFAULT_DIRECTORY = 'store'
+
+    @staticmethod
+    def store(neural_networks: List[NeuralNetwork],
+              file_name: str,
+              directory_name: str = _DEFAULT_DIRECTORY) -> None:
+        """
+        Stores neural networks in binary file with .nn extension.
+
+        Args:
+            neural_networks (List[NeuralNetwork]): Neural networks to store in file.
+            file_name (str): Name of file to store neural networks in,
+                will be created if doesn't exist.
+            directory_name (str): Name of directory where file is,
+                will be created if doesn't exist.
+        """
+        if not exists(directory_name):
+            makedirs(directory_name)
+        with open(f'{directory_name}/{file_name}.nn', 'wb') as store:
+            for neural_network in neural_networks:
+                pickle.dump(neural_network, store, pickle.HIGHEST_PROTOCOL)
+
+    @staticmethod
+    def load(file_name: str,
+             directory_name: str = _DEFAULT_DIRECTORY,
+             amount_to_load: int = -1) -> List[NeuralNetwork]:
+        """
+        Loads neural networks from binary file with .nn extension.
+
+        Args:
+            file_name (str): Name of file to load neural networks from
+            directory_name (str): Name of directory where file to load is.
+            amount_to_load (int): Maximal amount of neural networks to load,
+                if there is no enough neural networks in file,
+                everything will be loaded without any exception,
+                same for negative value.
+        Raises:
+            FileNotFoundError: If file or directory name doesn't exist.
+            UnpicklingError: If inappropriate file is given.
+        """
+        neural_networks = []
+        with open(f'{directory_name}/{file_name}.nn', 'rb') as store:
+            counter = 0
+            while counter != amount_to_load:
+                try:
+                    neural_networks.append(pickle.load(store))
+                except EOFError:
+                    break
+                counter += 1
+        return neural_networks
